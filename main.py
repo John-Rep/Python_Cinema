563ebbb2fe16b98f3c3a54f2fff3ddb7f82754da
John-Rep <replogle.john@next-u.fr>
2025-01-09 18:16:14 +0100

    Exercice #2

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #Commencer Exercice #1
 
 import pandas as pd
+#import numpy as np
+import matplotlib.pyplot as plt
+
 data = pd.read_csv("cinemas.csv")
 pd.set_option("display.max_columns", None)
 print(data.isnull().sum())
@@ -28,3 +31,21 @@ print(data.head())
 print(data.loc[:,['écrans','fauteuils','entrées 2022','entrées 2021']].describe())
 
 
+
+#Commencer Exercice #2
+
+data_com = data.groupby('commune')
+print(data_com.describe())
+data['entrées par fauteuil'] = data['entrées 2022'] / data['fauteuils']
+print(data.nlargest(3,'entrées par fauteuil'))
+print(data.nsmallest(3,'entrées par fauteuil'))
+
+
+data_epf = data.nlargest(10, 'entrées par fauteuil')
+epf = data_epf.plot(kind = 'bar', x='nom', y='entrées par fauteuil', figsize=(12,6), rot = 45, legend=False)
+epf.set_xlabel('Nom du cinéma', fontsize=16)
+epf.set_ylabel('Entrées par fauteuil', fontsize=16)
+epf.set_title('Entrées par fauteuil des cinémas', fontsize=20)
+plt.setp(epf.get_xticklabels(), ha='right')
+plt.subplots_adjust(bottom=.4)
+plt.show()
