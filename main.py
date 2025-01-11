a4e51db2ce026446882faf173a985aa7b7cc3335
John-Rep <replogle.john@next-u.fr>
2025-01-11 21:21:49 +0100

    Exercice #2 : mise à jour

--- main.py
+++ main.py
@@ -34,18 +34,22 @@ print(data.loc[:,['écrans','fauteuils','entrées 2022','entrées 2021']].descri
 
 #Commencer Exercice #2
 
-data_com = data.groupby('commune')
-print(data_com.describe())
-data['entrées par fauteuil'] = data['entrées 2022'] / data['fauteuils']
-print(data.nlargest(3,'entrées par fauteuil'))
-print(data.nsmallest(3,'entrées par fauteuil'))
+#Grouper les lignes de data par commune
+data_com = data.groupby('commune').sum().reset_index()
 
+#Calculer les entrées moyennes par fauteuil pour chaque commune
+data_com['entrées par fauteuil'] = data_com['entrées 2022'] / data_com['fauteuils']
+#Afficher les données des trois communes avec le plus et le moins d'entrées par fauteuil
+print(data_com.nlargest(3,'entrées par fauteuil'))
+print(data_com.nsmallest(3,'entrées par fauteuil'))
 
-data_epf = data.nlargest(10, 'entrées par fauteuil')
-epf = data_epf.plot(kind = 'bar', x='nom', y='entrées par fauteuil', figsize=(12,6), rot = 45, legend=False)
-epf.set_xlabel('Nom du cinéma', fontsize=16)
+#Séparer les 10 communes avec le plus d'entrées par fauteuil et faire un graphique
+data_epf = data_com.nlargest(10, 'entrées par fauteuil')
+epf = data_epf.plot(kind = 'bar', x='commune', y='entrées par fauteuil', figsize=(12,6), rot = 45, legend=False)
+#Ajuster l'affichage du graphique pour le rendre plus lisible
+epf.set_xlabel('Nom de la commune', fontsize=16)
 epf.set_ylabel('Entrées par fauteuil', fontsize=16)
-epf.set_title('Entrées par fauteuil des cinémas', fontsize=20)
+epf.set_title('Entrées par fauteuil des communes', fontsize=20)
 plt.setp(epf.get_xticklabels(), ha='right')
 plt.subplots_adjust(bottom=.4)
 plt.show()
@@ -62,7 +66,7 @@ faut_corr = data['fauteuils'].corr(data['entrées 2022'])
 print("Corrélation entre écrans et entrées en 2022 : {}".format(ecran_corr))
 print("Corrélation entre fauteuils et entrées en 2022 : {}".format(faut_corr))
 
-#Transformer les entrées pour rendre les valeurs plus lisibles dans une graphique
+#Transformer les entrées pour rendre les valeurs plus lisibles dans un graphique
 data['entrées 2022 plot'] = data['entrées 2022'] / 1000000
 #Générer un nuage de points pour écrans vs entrées
 data.plot(kind='scatter', x='écrans', y='entrées 2022 plot', alpha=.3,
